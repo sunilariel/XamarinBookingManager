99dd5a4c1ca6e996f8177631f54d7b6ab9fdc022
ariel.amitc <ariel.amitc@gmail.com>
2018-06-27 16:33:40 +0530

    no message

--- Demo App/Demo_App.Android/obj/Debug/__library_projects__/Telerik.Xamarin.Android.Primitives/library_project_imports/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\_J\workspace\Binaries_Xamarin_Android_xamarin\AndroidSource\Primitives\src\main\bin\AndroidManifest.xml : \
-C:\_J\workspace\Binaries_Xamarin_Android_xamarin\AndroidSource\Primitives\src\main\AndroidManifest.xml \
